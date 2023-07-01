dc3e49e80f723ddc169f6303bf7ef45f52bb5ca0
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2023-07-01 10:27:19 +0300

    Changed to top-down view and health bars follow camera

--- Elfshock Dungeon Crawler/Assets/Scripts/HealthBar.cs
+++ Elfshock Dungeon Crawler/Assets/Scripts/HealthBar.cs
@@ -5,11 +5,22 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+
     private Slider slider;
+    private Camera cam;
 
     void Awake()
     {
         slider = GetComponent<Slider>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        transform.rotation = cam.transform.rotation;
+        transform.position = target.position + offset;
     }
 
     public void UpdateHealth(float currentValue, float maxValue)
